440907e3a885eac6c367e2881ddbd1fd408c2e84
Alex Bradley <a.bradley.dm@gmail.com>
2024-12-29 01:12:37 +0000

    Readonly enums

--- src/app/model/thyrannic-day.ts
+++ src/app/model/thyrannic-day.ts
@@ -5,14 +5,14 @@ export class TDay {
     readonly name: string
   ) {}
 
-  public static DOLGOS: TDay = new TDay(1, 'Dolgos');
-  public static NAUGOS: TDay = new TDay(2, 'Naugos');
-  public static BROGOS: TDay = new TDay(3, 'Brogos');
-  public static THYRGOS: TDay = new TDay(4, 'Śyrgos');
-  public static DRAXIGOS: TDay = new TDay(5, 'Draxigos');
-  public static TELUGOS: TDay = new TDay(6, 'Telugos');
+  public static readonly DOLGOS: TDay = new TDay(1, 'Dolgos');
+  public static readonly NAUGOS: TDay = new TDay(2, 'Naugos');
+  public static readonly BROGOS: TDay = new TDay(3, 'Brogos');
+  public static readonly THYRGOS: TDay = new TDay(4, 'Śyrgos');
+  public static readonly DRAXIGOS: TDay = new TDay(5, 'Draxigos');
+  public static readonly TELUGOS: TDay = new TDay(6, 'Telugos');
 
-  public static values: TDay[] = [this.DOLGOS, this.NAUGOS, this.BROGOS, this.THYRGOS, this.DRAXIGOS, this.TELUGOS];
+  public static readonly values: TDay[] = [this.DOLGOS, this.NAUGOS, this.BROGOS, this.THYRGOS, this.DRAXIGOS, this.TELUGOS];
 
   public valueOf(): number {
     return this.id - 1;

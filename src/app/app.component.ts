0f70a6001c55a860336ee83770c7bacf3f46e71a
Alex Bradley <a.bradley.dm@gmail.com>
2025-01-27 23:39:23 +0000

    Save datetime in local storage (closes #17)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { TemporalUnit } from './model/temporal-unit';
 import { TDate } from './model/thyrannic-date';
 import { TDateTime } from './model/thyrannic-date-time';
 import { OrdinalPipe } from './pipes/ordinal';
+import { LocalStorage } from './util/local-storage';
 
 @Component({
   selector: 'app-root',
@@ -27,26 +28,38 @@ export class AppComponent {
   protected readonly units = TemporalUnit;
   protected readonly cities: Array<City> = City.values;
 
-  protected _datetime: TDateTime = TDate.fromDate().at(12, 0);
-  public get datetime(): TDateTime { return this._datetime; }
+  private _datetime: TDateTime;
+  public get datetime(): TDateTime {
+    return this._datetime;
+  }
+  // Update datetime and set in local storage
+  protected set datetime(datetime: TDateTime) {
+    this._datetime = datetime;
+    LocalStorage.CURRENT_DATETIME.put(datetime.valueOf().toString());
+  }
   
   protected _city: City = City.THYRANNOS;
   public get city(): City { return this._city; }
   
   constructor() {
     AppComponent.instance = this;
+    
+    // Load last datetime from storage
+    const storedValue = LocalStorage.CURRENT_DATETIME.get();
+    if (storedValue !== null) {
+      this._datetime = TDateTime.fromValue(Number(storedValue));
+    } else {
+      // Default value
+      this._datetime = TDate.fromDate().at(12, 0);
+    }
   }
   
   public changeDateTime([quantity, unit]: [number, TemporalUnit]) {
     try {
-      this._datetime = this.datetime.add(quantity, unit);
+      this.datetime = this.datetime.add(quantity, unit);
     } catch (err) {
       console.error('Illegal operation:', err);
     }
-  }
-  
-  public setDateTime(datetime: TDateTime) {
-    this._datetime = datetime;
   }
   
   public changeCity() {

ac14985f5e569ba006060128438d7df56b4ac592
Alex Bradley <a.bradley.dm@gmail.com>
2025-03-21 01:43:09 +0000

    Remove redundant %

--- src/app/components/celestial-bg/earth/earth.component.ts
+++ src/app/components/celestial-bg/earth/earth.component.ts
@@ -87,7 +87,7 @@ export class EarthComponent {
     const { declination } = OrbitalMechanics.computeRaDec(CelestialBody.sun, datetime.add(-6, TemporalUnit.WEEK));
     const dayLength = OrbitalMechanics.getDayLength(declination);
     const progress = MathUtil.tween(10, dayLength, 8);
-    this.groundColor = `color-mix(in xyz, ${100 * (1-progress)}% green, ${100 * progress}% snow)`;
+    this.groundColor = `color-mix(in xyz, green, ${100 * progress}% snow)`;
   }
 
   public updateTerrain() {

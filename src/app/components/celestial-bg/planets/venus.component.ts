5932c653b3820a716dc87274cc340bdce7055af7
Alex Bradley <a.bradley.dm@gmail.com>
2025-03-30 22:39:43 +0100

    Add units to Venus

--- src/app/components/celestial-bg/planets/venus.component.ts
+++ src/app/components/celestial-bg/planets/venus.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { AU, days, km } from "src/app/util/units";
+import { AU, days, deg, km } from "src/app/util/units";
 import { CelestialBg } from "../celestial-bg.component";
 import { HeliocentricBody, IntrasolarBody } from "../celestial-body/intrasolar-body";
 
@@ -14,11 +14,11 @@ export class VenusComponent extends HeliocentricBody {
   override brightness: number = 0.4;
   override zIndex: number = 2;
 
-  override inclination: number = 3.3946;
-  override ascendingNodeLongitude: number = 76.6799;
-  override periapsisArgument: number = 54.8910;
+  override inclination: number = 3.3946 * deg;
+  override ascendingNodeLongitude: number = 76.6799 * deg;
+  override periapsisArgument: number = 54.8910 * deg;
   override eccentricity: number = 0.006773;
-  override originAngle: number = 179.5761;
+  override originAngle: number = 179.5761 * deg;
   override orbitalPeriod: number = 224.701 * days;
   override meanDistance: number = 0.72333 * AU;
   override radius: number = 6051.8 * km;
